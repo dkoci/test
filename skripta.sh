5554de05fc721cbcc8b34112c7e29490574a304c
Davorin <davorin85@gmail.com>
2023-07-10 12:59:16 +0200

    Update skripta.sh

--- skripta.sh
+++ skripta.sh
@@ -1,4 +1,4 @@
 curl -LO https://github.com/aquasecurity/trivy/releases/download/v0.43.1/trivy_0.43.1_Linux-64bit.tar.gz
 tar -xzf ./trivy_0.43.1_Linux-64bit.tar.gz
 chmod +x ./trivy
-trivy image example:latest
+./trivy image example:latest
